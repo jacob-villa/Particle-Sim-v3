2fc4390ce208561110430820cf8549f4ee325409
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2024-04-06 04:17:24 +0800

    update: Sprite class

--- java_client/JavaClient/src/Sprite.java
+++ java_client/JavaClient/src/Sprite.java
@@ -1,3 +1,23 @@
 public class Sprite {
+    float x, y;
+    float speed;
 
+    Sprite(float x, float y, float speed){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    void Move(float dx, float dy) {
+        x += dx;
+        y -= dy;
+
+        if (x < 0) x = 0;
+        if (x > 1280) x = 1280;
+        if (y < 0) y = 0;
+        if (y > 720) y = 720;
+
+        x = Math.max(0.0f, Math.min(1280.0f, x));
+        y = Math.max(0.0f, Math.min(720.0f, y));
+    }
 }
