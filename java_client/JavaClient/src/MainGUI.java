5e2acde37b5c3bebacb739467ef7030a87a2103d
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2024-04-07 01:11:50 +0800

    update: GUI changes

--- java_client/JavaClient/src/MainGUI.java
+++ java_client/JavaClient/src/MainGUI.java
@@ -1,11 +1,49 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MainGUI {
-    static JFrame mainWindow = new JFrame();
+    public static final JFrame mainWindow = new JFrame();
     static JPanel parentPanel = new JPanel();
-    static ParticlesDrawArea simulationPanel = new ParticlesDrawArea();
+    private static final ParticlesDrawArea simulationPanel = new ParticlesDrawArea();
 
-    MainGUI(){
+    public static void initializeGUI() {
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        parentPanel.setSize(
+                (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(),
+                (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()
+        );
+        parentPanel.setBackground(Color.GRAY);
+        parentPanel.add(simulationPanel);
 
+        simulationPanel.setPreferredSize(
+                new Dimension(
+                        getScaledWidth(1280),
+                        getScaledHeight(720)
+                )
+        );
+
+        // Add panel to the center of the content pane
+        mainWindow.getContentPane().add(parentPanel, BorderLayout.CENTER);
+        mainWindow.setSize(
+                (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(),
+                (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()
+        );
+        mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        mainWindow.setVisible(true);
+    }
+
+    public static int getScaledWidth(int width){
+        int absoluteScreenWidth = mainWindow.getGraphicsConfiguration().getDevice().getDisplayMode().getWidth();
+        int scaledScreenWidth = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+
+        return width * scaledScreenWidth / absoluteScreenWidth;
+    }
+
+    public static int getScaledHeight(int height){
+        int absoluteScreenHeight = mainWindow.getGraphicsConfiguration().getDevice().getDisplayMode().getHeight();
+        int scaledScreenHeight = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+
+        return height * scaledScreenHeight / absoluteScreenHeight;
     }
 }
