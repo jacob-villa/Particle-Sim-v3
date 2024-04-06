0a7ee2e154b379ea424f5d1778ee0bb4cd1a9748
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2024-04-06 17:32:42 +0800

    add: task queue

--- java_client/JavaClient/src/Main.java
+++ java_client/JavaClient/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.DelayQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -5,6 +6,7 @@ public class Main {
 
     static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors();
     public static ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
+    public static DelayQueue<Task> tasksQueue = new DelayQueue<>();
 
 
     public static void main(String[] args) {
