4915f79d13dbdd6cc0ddb74a9123c34d4410f1f9
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2024-04-07 17:29:25 +0800

    update: moving sprite (no zoom)

--- java_client/JavaClient/src/ParticlesDrawArea.java
+++ java_client/JavaClient/src/ParticlesDrawArea.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ParticlesDrawArea extends JPanel {
@@ -20,6 +21,7 @@ public class ParticlesDrawArea extends JPanel {
     static CopyOnWriteArrayList<Particle> particles = new CopyOnWriteArrayList<>();
 
     public Graphics2D g2d = null;
+    private int ovalSize = 10;
 
     ParticlesDrawArea() {
         super();
@@ -31,6 +33,35 @@ public class ParticlesDrawArea extends JPanel {
         super.paintComponent(g);
 
         g2d = (Graphics2D) g.create();
+
+//        g2d.translate(
+//                MainGUI.getScaledWidth((int)userSprite.x),
+//                MainGUI.getScaledHeight((int)userSprite.y)
+//        );
+//
+//        g2d.setTransform(new AffineTransform());
+//        g2d.scale(
+//                (double) getWidth() / MainGUI.getScaledWidth(33 * ovalSize),
+//                (double) getHeight() / MainGUI.getScaledHeight(19 * ovalSize)
+//        );
+//
+//        g2d.setColor(Color.WHITE);
+//        g2d.fillRect(MainGUI.getScaledWidth(-144), MainGUI.getScaledHeight(-81), MainGUI.getScaledWidth(144), MainGUI.getScaledHeight(850));
+//        g2d.fillRect(MainGUI.getScaledWidth(1280), MainGUI.getScaledHeight(-81), MainGUI.getScaledWidth(144), MainGUI.getScaledHeight(850));
+//        g2d.fillRect(MainGUI.getScaledWidth(-144), MainGUI.getScaledHeight(-81), MainGUI.getScaledWidth(1280 + 144), MainGUI.getScaledHeight(81));
+//        g2d.fillRect(MainGUI.getScaledWidth(-144), MainGUI.getScaledHeight(720), MainGUI.getScaledWidth(1280 + 144), MainGUI.getScaledHeight(81));
+//
+//
+//
+//        g2d.fillRect(MainGUI.getScaledWidth(-9999), MainGUI.getScaledHeight(-9999), MainGUI.getScaledWidth(9999), MainGUI.getScaledHeight(9999));
+
+        g2d.setColor(Color.BLUE);
+        g2d.fillOval(
+                MainGUI.getScaledWidth((int)userSprite.x),
+                MainGUI.getScaledHeight(720) - MainGUI.getScaledHeight((int)userSprite.y),
+                ovalSize,
+                ovalSize
+        );
     }
 
 
